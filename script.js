d867a9981cefe8357ccde4e8c813cf6b90084005
Ajay Kumar <65358216+AjayKumar59@users.noreply.github.com>
2021-09-20 10:01:55 +0530

    Ud

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["FrontEnd Developer", "WordPress Developer", "Freelancer", "Blogger"],
+        strings: ["FrontEnd Developer", "WordPress Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
